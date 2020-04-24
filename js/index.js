1e4fb6fdf5460abd475a3186c7ccd15a2c6dc9c9
CodeFr33k <noreply>
2020-04-24 19:51:27 +0930

    create time interval

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-import("../pkg/index.js").catch(console.error);
+import('../pkg/index.js').catch(console.error);
+
+function setTime() {
+    const now = new Date();
+    const local = Intl.DateTimeFormat('en-US', {
+        hour: '2-digit',
+        hourCycle: 'h24',
+        minute: '2-digit',
+        second: '2-digit',
+    }).format(now);
+    const utc = Intl.DateTimeFormat('en-US', {
+        hour: '2-digit',
+        hourCycle: 'h24',
+        minute: '2-digit',
+        second: '2-digit',
+        timeZone: 'UTC',
+    }).format(now);
+    document.body.innerText = local + ' ' + utc + 'Z';
+}
+
+setInterval(setTime, 1000);
+setTime();
