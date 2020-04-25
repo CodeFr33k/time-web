1804d1a6a4edfad1046e32df22a7e854fbae2a20
CodeFr33k <noreply>
2020-04-25 12:29:26 +0930

    change local time to 12 hour cycle

--- js/index.js
+++ js/index.js
@@ -3,10 +3,10 @@ import('../pkg/index.js').catch(console.error);
 function setTime() {
     const now = new Date();
     const local = Intl.DateTimeFormat('en-US', {
-        hour: '2-digit',
-        hourCycle: 'h24',
+        hour: 'numeric',
+        hourCycle: 'h11',
         minute: '2-digit',
-        second: '2-digit',
+        second: '2-digit',        
     }).format(now);
     const utc = Intl.DateTimeFormat('en-US', {
         hour: '2-digit',
